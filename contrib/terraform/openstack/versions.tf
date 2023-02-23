5b3d5b26fbe0f0aad06d16ee6a4488476e2d29da
davidumea <david.andersson@elastisys.com>
2023-02-23 10:54:44 +0100

    Include template provider for migration

--- contrib/terraform/openstack/versions.tf
+++ contrib/terraform/openstack/versions.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "terraform-provider-openstack/openstack"
       version = "~> 1.17"
     }
+
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
+    }
   }
   experiments      = [module_variable_optional_attrs]
   required_version = ">= 0.14.0"
