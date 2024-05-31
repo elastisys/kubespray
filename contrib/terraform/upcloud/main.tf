cb87ec50257e7d44176ef7693bdb5c4447afb388
davidumea <david.andersson@elastisys.com>
2024-05-31 14:58:44 +0200

    Add support for LB in upcloud private zone

--- contrib/terraform/upcloud/main.tf
+++ contrib/terraform/upcloud/main.tf
@@ -12,7 +12,9 @@ module "kubernetes" {
   source = "./modules/kubernetes-cluster"
 
   prefix = var.prefix
-  zone   = var.zone
+  zone          = var.zone
+  private_cloud = var.private_cloud
+  public_zone   = var.public_zone
 
   template_name = var.template_name
   username      = var.username
