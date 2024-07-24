d43c41f42e7a8c5a7993417e150f016d5656fa08
Fredrik Liv <fredrik.liv@elastisys.com>
2024-07-24 16:54:45 +0200

    Added support for deprecated network field for UpCloud loadbalancers

--- contrib/terraform/upcloud/modules/kubernetes-cluster/main.tf
+++ contrib/terraform/upcloud/modules/kubernetes-cluster/main.tf
@@ -546,16 +546,27 @@ resource "upcloud_loadbalancer" "lb" {
   name              = "${local.resource-prefix}lb"
   plan              = var.loadbalancer_plan
   zone              = var.private_cloud ? var.public_zone : var.zone
-  networks {
-    name    = "Private-Net"
-    type    = "private"
-    family  = "IPv4"
-    network = upcloud_network.private.id
+  network           = var.loadbalancer_legacy_network ? upcloud_network.private.id : null
+
+  dynamic "networks" {
+    for_each = var.loadbalancer_legacy_network ? [] : [1]
+
+    content {
+      name    = "Private-Net"
+      type    = "private"
+      family  = "IPv4"
+      network = upcloud_network.private.id
+    }
   }
-  networks {
-    name   = "Public-Net"
-    type   = "public"
-    family = "IPv4"
+
+  dynamic "networks" {
+    for_each = var.loadbalancer_legacy_network ? [] : [1]
+
+    content {
+      name   = "Public-Net"
+      type   = "public"
+      family = "IPv4"
+    }
   }
 
   lifecycle {
