c90e6f065e1d02fac03bee71bc5c8f9709a9bd3d
Joy Johansson <joy.johansson@elastisys.com>
2026-03-18 12:12:00 +0100

    feat(terraform/upcloud): support floating IPs on load balancers

--- contrib/terraform/upcloud/modules/kubernetes-cluster/variables.tf
+++ contrib/terraform/upcloud/modules/kubernetes-cluster/variables.tf
@@ -113,8 +113,12 @@ variable "loadbalancers" {
     plan            = string
     legacy_network  = bool
     public_network  = bool
-    private_network = bool
-
+    private_network    = bool
+    create_floating_ip = optional(bool, false)
+    ip_addresses = optional(list(object({
+      address      = string
+      network_name = string
+    })), [])
     targets = map(object({
       proxy_protocol  = bool
       port            = number
